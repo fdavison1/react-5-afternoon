28b8e2026e532a77f213f5fa99376ece5f42234d
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-10-31 15:14:35 -0600

    finished assignment

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -23,6 +23,8 @@ const initialState = {
 const UPDATE_LOAN_TYPE = "UPDATE_LOAN_TYPE"
 const UPDATE_PROPERTY_TYPE = "UPDATE_PROPERTY_TYPE"
 const UPDATE_CITY = "UPDATE_CITY"
+const UPDATE_PROP = "UPDATE_PROP"
+const UPDATE_FOUND = "UPDATE_FOUND"
 
 //ACTION BUILDERS
 export function updateLoanType(loanType){
@@ -46,6 +48,20 @@ export function updateCity(city){
     }
 }
 
+export function updateProp(prop){
+    return {
+        type: UPDATE_PROP,
+        payload: prop
+    }
+}
+
+export function updateFound(found){
+    return {
+        type: UPDATE_FOUND,
+        payload: found
+    }
+}
+
 //REDUCER FUNCTION
 function reducer(state = initialState, action){
     switch (action.type){
@@ -55,6 +71,10 @@ function reducer(state = initialState, action){
             return Object.assign({}, state, {propertyType: action.payload})
         case UPDATE_CITY: 
             return Object.assign({}, state, {city: action.payload})
+        case UPDATE_PROP: 
+            return Object.assign({}, state, {propToBeUsedOn: action.payload})
+        case UPDATE_FOUND: 
+            return Object.assign({}, state, {found: action.payload})
         default: return state
     }
 }
