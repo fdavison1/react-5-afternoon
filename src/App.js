8d3524c796e0c60fbebf319f174dbb3102df54c2
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-10-31 13:15:48 -0600

    step 3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import router from './router'
-
+// import {HashRouter} from 'react-router-dom'
 
 class App extends Component {
   constructor(){
--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import store from './store'
+import {Provider} from 'react-redux'
+import {HashRouter} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+
+
+<Provider store={store}>
+<App />
+</Provider>
+
+
+
+
+, document.getElementById('root'));
 registerServiceWorker();
 
